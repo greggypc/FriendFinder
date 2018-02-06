c8553466ac8088461fe715f3ccfbd71b06048cf1
Greg Church <greggypc@users.noreply.github.com>
2018-02-05 22:14:27 -0600

    add test data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,21 +9,13 @@ var friendsData = require("../data/friends");
 
 module.exports = function(app) {
     // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
   
     app.get("/api/friends", function(req, res) {
       res.json(friendsData);
     });
   
     app.post("/api/friends", function(req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body-parser middleware
-       
-      // var friendsArray = [2,3,2,6,3,2,1,4,3,2];
+       //test scores
         var friendsArray = [
             {scores: [5,4,3,2,4,5,2,1,3,4]
             },
@@ -48,13 +40,5 @@ module.exports = function(app) {
         //friendsData.push(req.body);
         //res.json(true);
         }
-
-        // for(var i = 0; i <= b.length-1; i++)
-        // x.push(Math.abs(a[i] -b[i]));
-        // console.log(x);
-
-        // friendsData.push(req.body);
-        //   res.json(true);
-       
       });    
 };      
