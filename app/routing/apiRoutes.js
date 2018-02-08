4f1db74976465fdf5c16b66b0f15d7860f4d1d1d
Greg Church <greggypc@users.noreply.github.com>
2018-02-07 23:17:43 -0600

    logic good. Array still coming in as string - need to de-stringafy

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,42 +18,48 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
       var diff = [];
       var lowestArray = [];
-      var userData = req.body;
+      var best = 0;
+      var userData =  req.body;
       var userScores = userData.scores;
-     console.log(friendsArray);
-     friendsArray.push(userData);
       
-    //   var friendsArray = [
-    //     {scores: [5,4,3,2,4,5,2,1,3,4]
-    //     },
-    //     {scores: [2,0,5,6,3,2,4,0,2,6]
-    //     },
-    //     {scores: [6,5,2,3,4,1,5,6,3,2]
-    //     }
-    //   ];
-    // var userData = [2,0,5,6,3,2,4,0,2,6];
+     console.log(userScores);
+     friendsArray.push(userData);
+
+    for(var i = 0; i <= friendsArray.length-1; i++) {
+      var diff = friendsArray[i].scores.map(function(item, index) {
+        // item is currentValue of friendsArray
+        // using index to get value from array userScores
+        // return difference of each set of scores to array diff
+        return Math.abs(item - userScores[index]);
+      })
+      console.log(diff);
+
+      // reduce diff to a single number total
+      var total = diff.reduce(function (prev, current) {
+        return prev + current;
+    }, 0);
+
+    console.log(total);
+
+    // push each total into array of scores
+    lowestArray.push(total);
+
+    }; 
     
-
-//     for(var i = 0; i <= friendsData.length-1; i++) {
-//   var diff = friendsData[i].scores.map(function(item, index) {
-//     // In this case item correspond to currentValue of array a, 
-//     // using index to get value from array b
-//     return Math.abs(item - userScores[index]);
-//   })
-//   console.log(diff);
-//   var total = diff.reduce(function (prev, current) {
-//     return prev + current;
-// }, 0);
-
-// console.log(total);
-// lowestArray.push(total);
-
-// }; 
-// console.log(lowestArray);
-//console.log(Math.min(...lowestArray));
- 
-         res.json(userData);
-         res.json(userData);
+    console.log(lowestArray);
+    
+    // find lowest score in lowestArray
+    for (var j = 0; j < lowestArray; j++) {
+      if (lowestArray[i] <= lowestArray[best]) {
+        best = j;
+      }
+    }
+         // return name and photo of lowestscore/best match
+         res.json(friendsArray.name[best]);
+         res.json(friendsArray.photo[best]);
+         
+         // push newest customers data into friendsArray
+         friendsArray.push(userData);
        
         
       });  //end post    
