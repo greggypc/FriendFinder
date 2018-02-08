bc963785c7ee7220aa875ad9867bce3b84ee92fb
Greg Church <greggypc@users.noreply.github.com>
2018-02-08 17:25:29 -0600

    help

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,6 +17,7 @@ module.exports = function(app) {
   
     app.post("/api/friends", function(req, res) {
       var diff = [];
+      var total = 0;
       var lowestArray = [];
       var best = 0;
       var userData =  req.body;
@@ -25,8 +26,12 @@ module.exports = function(app) {
      //console.log(userScores);
      friendsArray.push(userData);
 
+    // for every entry in friendsArray:
     for(var i = 0; i <= friendsArray.length-1; i++) {
-      var diff = friendsArray[i].scores.map(function(item, index) {
+
+      // subtract each value in friendsArray score from corresponding userData score
+      // this becomes new array diff
+        diff = friendsArray[i].scores.map(function(item, index) {
         // item is current value of friendsArray
         // using index to get value from array userScores
         // return difference of each set of scores to array diff
@@ -35,13 +40,13 @@ module.exports = function(app) {
       console.log(diff);
 
       // reduce diff to a single number total
-      var total = diff.reduce(function (prev, current) {
+      total = diff.reduce(function (prev, current) {
         return prev + current;
     }, 0);
 
     console.log(total);
 
-    // push each total into array of scores
+    // push each total into lowestArray of scores
     lowestArray.push(total);
 
     }; 
@@ -49,17 +54,19 @@ module.exports = function(app) {
     console.log(lowestArray);
     
     // find lowest score in lowestArray
-    for (var j = 0; j < lowestArray; j++) {
+    for (var j = 0; j < lowestArray.length; j++) {
       if (lowestArray[j] <= lowestArray[best]) {
         best = j;
       }
     }
+         // push newest customers data into friendsArray
+         friendsArray.push(userData);
+         
          // return name and photo of lowestscore/best match
          res.json(friendsArray.name[best]);
          res.json(friendsArray.photo[best]);
          
-         // push newest customers data into friendsArray
-         friendsArray.push(userData);
+         
        
         
       });  //end post    
