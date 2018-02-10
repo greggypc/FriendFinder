3eda8de8f99d73aaa683d0602ed51601c9c5e060
Greg Church <greggypc@users.noreply.github.com>
2018-02-09 21:17:04 -0600

    fin

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,23 +2,23 @@
 
 var friendsArray = [
     {
-      name: "greg",
-      photo: "http://media.tmz.com/2017/03/30/0330-greg-the-hammer-valentine-now-photos-launch-1.jpg",
+      name: "Greg",
+      photo: "http://d1726u9k08127k.cloudfront.net/wp-content/uploads/2012/04/Valentine.jpg",
       scores: [5,4,3,2,4,5,2,1,3,4]
     },
     {
-      name: "ted",
-      photo: "http://media.tmz.com/2017/03/30/0330-greg-the-hammer-valentine-now-photos-launch-1.jpg",
+      name: "Ted",
+      photo: "https://www.politico.com/design/magazine/politico50/2014/headshot-ted-cruz.jpg",
       scores: [5,4,3,2,4,5,2,1,3,4]
     },
     {
-      name: "john",
-      photo: "http://media.tmz.com/2017/03/30/0330-greg-the-hammer-valentine-now-photos-launch-1.jpg",
+      name: "Antony",
+      photo: "https://img.discogs.com/IGIxaUO3Xd2EKfzECX2Zs5Z-qaU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-185134-1128941460.jpeg.jpg",
       scores: [2,0,5,5,3,2,4,0,2,5]
     },
     {
-      name: "sue",
-      photo: "http://media.tmz.com/2017/03/30/0330-greg-the-hammer-valentine-now-photos-launch-1.jpg",
+      name: "Beatix",
+      photo: "https://img00.deviantart.net/93df/i/2004/221/a/a/beatrix_kiddo.jpg",
       scores: [5,5,2,3,4,1,5,5,3,2]
     }
   ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,8 @@
-// LOAD DATA
-// Link our routeto data source.
-// These data sources hold friends array
-// ===============================================================================
 
 var friendsArray = require("../data/friends");
 
-
-// ROUTING
-
 module.exports = function(app) {
-    // API GET Requests
-  
+   
     app.get("/api/friends", function(req, res) {
       res.json(friendsArray);
     });
@@ -20,11 +12,7 @@ module.exports = function(app) {
       var total = 0;
       var lowestArray = [];
       var best = 0;
-      var userData =  req.body;
-      var userScores = userData.scores;
-      
-     //console.log(userScores);
-     friendsArray.push(userData);
+      var userScores = req.body.scores;      
 
     // for every entry in friendsArray:
     for(var i = 0; i <= friendsArray.length-1; i++) {
@@ -37,37 +25,26 @@ module.exports = function(app) {
         // return difference of each set of scores to array diff
         return Math.abs(item - userScores[index]);
       })
-      console.log(diff);
 
       // reduce diff to a single number total
       total = diff.reduce(function (prev, current) {
         return prev + current;
     }, 0);
 
-    console.log(total);
-
     // push each total into lowestArray of scores
     lowestArray.push(total);
-
     }; 
-    
-    console.log(lowestArray);
-    
+   
     // find lowest score in lowestArray
     for (var j = 0; j < lowestArray.length; j++) {
       if (lowestArray[j] <= lowestArray[best]) {
         best = j;
       }
     }
-         // push newest customers data into friendsArray
-         friendsArray.push(userData);
-         
          // return name and photo of lowestscore/best match
-         res.json(friendsArray.name[best]);
-         res.json(friendsArray.photo[best]);
+         res.json(friendsArray[best]);
          
-         
-       
-        
-      });  //end post    
+         // push newest friend into friendsArray
+         friendsArray.push(req.body);      
+  });  //end post    
 };      
